5f49d635b09af2da6875307f0718dffc7ad134a1
antarikshmeshram <antarikshmeshram@gmail.com>
2017-11-25 12:31:37 +0530

    Modify the code - Add the generic code

--- session.js
+++ session.js
@@ -1,80 +1,63 @@
+var session = {storage: sessionStorage};
+var local = {storage: localStorage};
 
-document.getElementById("set").addEventListener("click", function(exp){
-    
-    var type = exp.target.id; 
-   
-    if(type=="b1" && document.getElementById("key1").value.length != 0){
-        sessionStorage.setItem(document.getElementById("key1").value,document.getElementById("value1").value);
-        alert("Stored in session storage");
-        
-    }
-    else if(type=="b2" && document.getElementById("key1").value.length != 0){
-        localStorage.setItem(document.getElementById("key1").value,document.getElementById("value1").value);
-        alert("Stored in local storage");
 
-    }
-    else if(type=="b3" && document.getElementById("key1").value.length != 0){
-        var cookieKey = document.getElementById("key1").value;
-        var cookieValue = document.getElementById("value1").value;
-        console.log(document.cookie = cookieKey + " " + cookieValue + " stored in cookie");
-        alert("Stored in cookie");
-    }
-},false);
+var setVal = function() {
+    var keyval = document.getElementById('key').value;
+    var value = document.getElementById('value').value;
+    this.storage.setItem(keyval,value);
+    alert("Data added!");
+}
 
-document.getElementById("get").addEventListener("click", function(exp){
-    var type = exp.target.id; 
-    //console.log(exp);
-    if(type=="b3" && document.getElementById("key2").value.length != 0){
-        var result = sessionStorage.getItem(document.getElementById("key2").value);
-        alert(result);
-    }
-    else if(type=="b4" && document.getElementById("key2").value.length != 0){
-        var result = localStorage.getItem(document.getElementById("key2").value);
-        alert(result);
+var setInLocal=setVal.bind(local);
+var setInSession=setVal.bind(session);
 
-    }
-},false);
 
-document.getElementById("list").addEventListener("click", function(exp){
-    var type = exp.target.id; 
-    if(type=="b5"){
-        for(var i = 0; i < sessionStorage.length; i++){
-         console.log(sessionStorage.getItem(sessionStorage.key(i)));    
+var clearStorage = function() {
+   this.storage.clear();
+   alert("The storage is cleared!");
+}
+
+var clearLocalStorage=clearStorage.bind(local);
+var clearSessionStorage=clearStorage.bind(session);
+
+
+var getVal = function() {
+      var x = this.storage.length;
+      if (x == 0)
+          alert("Storage is already empty");
+   else
+        for(var i = 0; i < x; i++) {
+            var key = this.storage.key(i);
+            var value = this.storage[key];
+            console.log("Key " + "" + key + " " +  "has value " + " " + value);
         }
-    }
-    else if(type=="b6"){
-        for(var i = 0; i < localStorage.length; i++){
-         console.log(localStorage.getItem(localStorage.key(i)));    
-        }
-    }
-},false);
+}
 
-document.getElementById("clear").addEventListener("click", function(exp){
-    var type = exp.target.id; 
-    //console.log(x);
-    if(type=="b7"){
-        sessionStorage.clear();
-        alert("Session storage cleared")
-    }
-    else if(type=="b8"){
-        localStorage.clear();
-        alert("Local storage cleared")
-
-    }
-},false);
-
-document.getElementById("remove").addEventListener("click", function(exp){
-    var type = exp.target.id; 
-    //console.log(x);
-    if(type=="b9" && document.getElementById("key3").value.length!=0){
-        sessionStorage.removeItem(document.getElementById("key3").value);
-        alert("Item removed");
-    }
-    else if(type=="b10" && document.getElementById("key3").value.length!=0){
-        localStorage.removeItem(document.getElementById("key3").value);
-        alert("Item removed");
-    }
-},false);
+var getFromLocal=getVal.bind(local);
+var getFromSession=getVal.bind(session);
 
 
+var getItems = function() {
+  var a = document.getElementById('key1').value;
+  console.log(a);
+  var p = this.storage.getItem(a);
+  console.log(p);
+  if (!p)
+      alert("Key-value pair does not exist!");
+  else
+      console.log("Key" + " " + a + " " + "has value" + " " + p);
+}
 
+var getSingleFromLocal=getItems.bind(local);
+var getSingleFromSession=getItems.bind(session);
+
+
+var remItem = function(){
+  var a = document.getElementById('key2').value;
+  this.storage.removeItem(a);
+  alert("Item removed");
+}
+
+var remFromLocal=remItem.bind(local);
+var remFromSession = remItem.bind(session);
